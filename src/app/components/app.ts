8c1b16ac05d4206f44d5094b4d8d61af9b0033f9
Maxim Salnikov <salnikov@gmail.com>
2015-12-07 22:07:18 +0100

    Optimization

--- src/app/components/app.ts
+++ src/app/components/app.ts
@@ -1,14 +1,13 @@
 import {bootstrap, Component} from 'angular2/angular2';
 import {EventListComponent} from './event-list';
 import {EventEditComponent} from './event-edit';
-import {EventService} from '../services/event';
 
 @Component({
   selector: 'app',
   template: `
     <h1>{{title}}</h1>
-    <event-list #event-list-ref class="layout-column-left"></event-list>
-    <event-edit [event]="eventListRef.selectedEvent" class="layout-column-right"></event-edit>
+    <event-list #event-list-ref class="layout-col-left"></event-list>
+    <event-edit [event]="eventListRef.selectedEvent" class="layout-col-right"></event-edit>
   `,
   directives: [EventListComponent, EventEditComponent]
 })
